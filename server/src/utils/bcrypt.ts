988923ff1f17777b255324fcdf2bb07e702d8d30
Adrián Iglesias Fernández <adriiglesias2016@gmail.com>
2026-01-06 12:47:32 +0100

    Instalación de CORS para realizar llamadas al Back-End

--- server/src/utils/bcrypt.ts
+++ server/src/utils/bcrypt.ts
@@ -2,7 +2,7 @@ import bcrypt from "bcrypt";
 
 // Crear
 export const hashPassword = async (password : string) => {
-    const salt = bcrypt.salt(10);
+    const salt = await bcrypt.genSalt(10);
     return await bcrypt.hash(password, salt)
 }
 
