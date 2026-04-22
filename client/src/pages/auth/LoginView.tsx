7f15933f1e3d893c9ed1bcfdfa9fdfe2149ed893
Adrián Iglesias Fernández <adriiglesias2016@gmail.com>
2026-04-22 13:24:37 +0200

    Close #29 Adaptar la vista Login y Register a los cambios actuales

--- client/src/pages/auth/LoginView.tsx
+++ client/src/pages/auth/LoginView.tsx
@@ -3,7 +3,7 @@ import LoginForm from "@/components/auth/LoginForm";
 export default function LoginView() {
   return (
     <div className="mt-10 p-10 mx-auto max-w-3xl">
-      <h1 className="text-center text-white font-black text-5xl">Inicia Sesión {'📕​'}</h1>
+      <h1 className="text-center text-[#C53F56] font-black text-5xl">Inicia Sesión {'📕​'}</h1>
       <div>
         <LoginForm />
       </div>
--- client/src/pages/auth/RegisterView.tsx
+++ client/src/pages/auth/RegisterView.tsx
@@ -4,7 +4,7 @@ import RegisterForm from "@/components/auth/RegisterForm";
 export default function RegisterView() {
   return (
     <div className="mt-10 p-10 mx-auto max-w-3xl">
-      <h1 className="text-center text-white font-black text-5xl">Crea tu perfil {'📕​'}</h1>
+      <h1 className="text-center text-[#C53F56] font-black text-5xl">Crea tu perfil {'📕​'}</h1>
       <div>
         <RegisterForm />
       </div>
